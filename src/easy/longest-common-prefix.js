5a43245169d48a71def0146264a5366716527a27
MagicCurlyHair <g.shokanov@gmail.com>
2020-02-17 23:08:03 +0300

    add longest common prefix solution

---
+++ src/easy/longest-common-prefix.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+/**
+ * Find longest common prefix among input strings
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  const prefix = [];
+  if (strs.length === 0) {
+    return '';
+  }
+  for (let i = 0; i < strs[0].length; i++) {
+    let value;
+    for (const str of strs) {
+      if (value && str[i] !== value) {
+        return prefix.join('');
+      }
+      value = str[i];
+    }
+    prefix.push(value);
+  }
+  return prefix.join('');
+}
+
+module.exports = {
+  longestCommonPrefix
+};
